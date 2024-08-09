cf66bf61d53d4f8cc954cee3b0a7e9a3ce209475
German <erick.manrique112@gmail.com>
2024-08-09 12:30:44 -0500

    Updated SEO, HTML, and JS for better site optimization

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@
 function criteria() { 
     const form = document.getElementById('contactForm');
     const validChars = ['.', '@', '_','-'];
-    let email = document.getElementById('email').value;    
+    let email = document.getElementById('email').value;
     let msg = document.getElementById('msg');
+    let name = document.getElementById('name').value;  
+    let phone = document.getElementById('phone').value;
     let firstAt = email.indexOf('@');
     let lastAt = email.lastIndexOf('@');
     let lastDot = email.lastIndexOf('.');
@@ -14,28 +16,34 @@ function criteria() {
     email = email.trim().toLowerCase();
     msg.innerHTML = '';
 
-    if(firstChar == '@' || firstChar == '.' || firstChar == '_' || firstChar == '-' || !isNaN(firstChar)){
+    if (name.trim() === "") {
+        msg.innerHTML = "Por favor, ingrese su nombre.";
+        state = false;
+    } else if (phone.trim() === "") {
+        msg.innerHTML = "Por favor, ingrese su teléfono.";
+        state = false;
+    } else if (firstChar == '@' || firstChar == '.' || firstChar == '_' || firstChar == '-' || !isNaN(firstChar)) {
         msg.innerHTML = "Invalid first character for Email address";
         state = false;
-    } else if(email.length < 8){
+    } else if (email.length < 8) {
         msg.innerHTML = "Your email is too short!";
         state = false;
-    } else if((firstAt < 2) || (firstAt != lastAt)){
+    } else if ((firstAt < 2) || (firstAt != lastAt)) {
         msg.innerHTML = "Error in @";
         state = false;
-    } else if(lastDot - lastAt < 3){
+    } else if (lastDot - lastAt < 3) {
         msg.innerHTML = "Error in domain name";
         state = false;
-    } else if(email.length - lastDot < 3){
+    } else if (email.length - lastDot < 3) {
         msg.innerHTML = "Error in .com";
         state = false;
     } else {
-        for(let i = 0; i < email.length && state == true; i++){
-            if((email.charCodeAt(i) >= 97 && email.charCodeAt(i) <= 122)){
+        for (let i = 0; i < email.length && state == true; i++) {
+            if ((email.charCodeAt(i) >= 97 && email.charCodeAt(i) <= 122)){
                 continue;
             } else if ((email.charCodeAt(i) >= 48 && email.charCodeAt(i) <= 57)) {
                 continue;
-            } else if (validChars.indexOf(email.charAt(i)) != -1){
+            } else if (validChars.indexOf(email.charAt(i)) != -1) {
                 continue;
             } else {
                 msg.innerHTML = "Please use valid email characters";
@@ -45,13 +53,13 @@ function criteria() {
     }
 
     if (state == true) {
-        msg.innerHTML = 'Thank You :) Your Message has been submitted successfully. <br> You shall hear from us very soon!';
+        msg.innerHTML = 'Muchas gracias :) Su mensaje ha sido recibido exitosamente <br> Muy pronto nos pondremos en contacto!';
         document.getElementById('email').classList.remove("invalid");
 
         // Form submission using FormSubmit.co
-        form.action = 'https://formsubmit.co/erick.manrique112@gmail.com';
+        form.action = 'https://formsubmit.co/dimidex.peru@gmail.com';
         form.method = 'POST';
-        form.submit();
+        HTMLFormElement.prototype.submit.call(form); // Submit the form
     } else {
         document.getElementById('email').classList.add("invalid");
     }
@@ -61,21 +69,4 @@ function criteria() {
 document.getElementById('contactForm').addEventListener("submit", (e) => {
     e.preventDefault();
     criteria();
-});
-
-// SEARCH functionality
-const searchElement = document.getElementById('search');
-const searchBar = document.getElementById('searchBar');
-// Click on the Magnifier icon to toggle the search bar
-searchElement.addEventListener('click', function (){
-    searchBar.classList.toggle('show');
-    searchBar.classList.toggle('hide');
-});
-// Press escape to close the search bar
-document.addEventListener('keydown', (event) => {
-    var keyName = event.key;
-    if ((keyName == 'Escape' && searchBar.classList.contains('show') == true)) {
-        searchBar.classList.toggle('show');
-        searchBar.classList.toggle('hide');    
-    }
 });
